b53e15e9a57e31071925ec35649c067ce3669ba6
Iwo <g00391231@atu.ie>
2023-11-15 09:48:44 +0000

    Person Validation

--- src/test/java/ie/atu/validation/Person.java
+++ src/test/java/ie/atu/validation/Person.java
@@ -1,16 +1,34 @@
 package ie.atu.validation;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class Person {
+    @NotBlank(message = "Name cannot be blank")
     private String name;
+
+    @NotBlank(message = "title cannot be blank")
+    @Size(min = 1, max = 3, message = "Is not valid Title")
     private String title;
+
+    @Min(value = 1, message = "Employee ID cannot be less than 1")
+    @NotNull(message = "Employee ID is required")
     private int employeeId;
+
+    @Min(value = 18, message = "Age cannot be less than 18")
+    @NotNull(message = "Age is required")
     private int age;
+
+    @NotBlank(message =  "Email is required")
+    @Email(message = "Not a valid email")
     private String email;
+
+    @NotBlank(message =  "position is  required")
     private String position;
+
+    @NotBlank(message =  "department is  required")
     private String department;
 }
