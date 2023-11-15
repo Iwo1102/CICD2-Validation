bb7be20aea14670718819ac2de3c7c3634350905
Iwo <g00391231@atu.ie>
2023-11-15 10:52:50 +0000

    Moved Classes from test to main folders

--- src/main/java/ie/atu/validation/PersonController.java
+++ src/main/java/ie/atu/validation/PersonController.java
@@ -3,7 +3,7 @@ package ie.atu.validation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+@RequestMapping("/person")
 @RestController
 public class PersonController {
     private final PersonService personService;
